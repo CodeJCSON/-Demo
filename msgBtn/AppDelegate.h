269c6c4596e1f8bbeab0d2ed0f341e957ba632dc
向彪 <xiangbiao@xiangbiaodemacbook-pro.local>
2015-12-09 09:57:42 +0800

    Initial Commit

---
+++ msgBtn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  msgBtn
+//
+//  Created by 向彪 on 15/12/9.
+//  Copyright © 2015年 Ritchie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ msgBtn/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  msgBtn
+//
+//  Created by 向彪 on 15/12/9.
+//  Copyright © 2015年 Ritchie. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
